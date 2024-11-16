6320ea1abf9db9d9a52935754d87adfd66bc5893
matcb <mathcarvalhobarreto@gmail.com>
2024-11-16 17:57:18 -0300

    commint useState() updates and useEffect() - probably final commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import {useEffect, useState} from 'react'
 import './App.css'
-import  searchIcon from './search.svg'
+import searchIcon from './search.svg'
 import MovieCard from './MovieCard'
 const API_URL = 'http://www.omdbapi.com/?apikey=1f53b3ab'
 
-
-
 function App() {
-
   const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('Superman')
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
@@ -16,38 +14,40 @@ function App() {
     setMovies(data.Search)
   }
 
+  useEffect(() => {
+    searchMovies(searchTerm)
+  }, []) // This will fetch movies when component mounts
 
   return (
     <div className='app'>
-      
       <h1>FilmPire</h1>
       
       <div className="search">
-        <input placeholder='Search a Movie' value='Superman' onChange={() => {}} />
-        <img src={searchIcon} alt='search' onClick={() => {}}/>
+        <input 
+          placeholder='Search a Movie' 
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img 
+          src={searchIcon} 
+          alt='search' 
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
 
-      {
-          movies?.length > 0 ?  (
-         <div className='container'>
-            {
-            movies.map((movie) => (
-              <MovieCard movie={movie} key={movie.imdbID} />
-            ))}
-          </div>
-        
-        ):(
-        
-        <div className='empty'><h2>No movie found</h2></div>
-
-        )}
-
-    
-
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No movie found</h2>
+        </div>
+      )}
     </div>
   )
 }
-
-
 
 export default App
